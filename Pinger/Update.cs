fbd5ac5dc5125e688cf1c67bd476d054012b3015
Infarh <shmachilin@gmail.com>
2025-03-08 17:04:22 +0300

    Автообновление

---
+++ Pinger/Update.cs
@@ -0,0 +1,11 @@
+﻿namespace Pinger;
+
+internal static class Update
+{
+    private static string __RepositoryUri = "https://github.com/Infarh/Pinger";
+
+    public static async Task CheckUpdateAsync(CancellationToken Cancel = default)
+    {
+
+    }
+}
